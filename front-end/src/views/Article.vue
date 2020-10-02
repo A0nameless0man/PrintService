110d5b4985ff1eae2eb213aca15ee43ac1e33fa0
HuGuang@NewDesktop <1395943920@qq.com>
2020-10-02 20:38:40 +0800

    redo frontend

---
+++ front-end/src/views/Article.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+        <a-button type="primary">{{ $route.params.id }}</a-button>
+    </div>
+</template>
+
+<script>
+import { Prop } from "vue-property-decorator";
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Button } from "ant-design-vue";
+
+@Component({ components: { AButton: Button } })
+export default class Article extends Vue {
+    article = {};
+    
+}
+</script>
