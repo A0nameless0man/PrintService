d67fc6ffa1d68b550d14847f4db37fe4d9dcd0ff
HuGuang@NewDesktop <1395943920@qq.com>
2020-10-02 21:04:49 +0800

    on track

---
+++ front-end/src/config.js
@@ -0,0 +1,1 @@
+export default { host: { backend: "https://10.0.1.128:3000" } };
--- front-end/src/views/Article.vue
+++ front-end/src/views/Article.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
         <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-        <a-button type="primary">{{ $route.params.id }}</a-button>
+        <a-button type="primary">{{ JSON.stringify(article) }}</a-button>
     </div>
 </template>
 
@@ -10,10 +10,19 @@ import { Prop } from "vue-property-decorator";
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Button } from "ant-design-vue";
-
+import config from "../config";
+import axios from "axios";
 @Component({ components: { AButton: Button } })
 export default class Article extends Vue {
+    // article = { id: this.$route.params.id };
     article = {};
-    
+    async created() {
+        console.log(config.host.backend);
+        this.article = (
+            await axios.get("/article/" + this.$route.params.id, {
+                baseURL: "http://10.0.1.128:3000"
+            })
+        ).data;
+    }
 }
 </script>
