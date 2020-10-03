3b796c63b8de10fad5efb8a1575186eebba1a07a
HuGuang@NewDesktop <1395943920@qq.com>
2020-10-03 15:22:55 +0800

    Markdown

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,60 @@
 <template>
     <div id="app">
-        <div>{{ JSON.stringify(articles) }}</div>
-        <a-menu v-model="currentMenu" mode="horizontal">
-            <a-menu-item key="Home"
-                ><router-link to="/">Home</router-link></a-menu-item
-            >
-            <a-menu-item key="About"
-                ><router-link to="/about">About</router-link></a-menu-item
-            >
-            <a-sub-menu>
-                <span slot="title" class="submenu-title-wrapper">
-                    <a-icon type="book" />Articles
-                </span>
-                <a-menu-item v-for="article in articles" v-bind:key="article.id"
-                    ><router-link :to="'/article/' + article.id">{{
-                        article.title
-                    }}</router-link></a-menu-item
-                >
-            </a-sub-menu>
-        </a-menu>
+        <a-layout>
+            <a-layout-header class="header">
+                <a-menu theme="dark" v-model="currentMenu" mode="horizontal">
+                    <a-menu-item key="Home">
+                        <router-link to="/">Home</router-link>
+                    </a-menu-item>
+                    <a-menu-item key="About">
+                        <router-link to="/about">About</router-link>
+                    </a-menu-item>
+                    <a-sub-menu>
+                        <span slot="title" class="submenu-title-wrapper">
+                            <a-icon type="book" />Articles
+                        </span>
+                        <a-menu-item
+                            v-for="article in articles"
+                            v-bind:key="article.id"
+                        >
+                            <router-link :to="'/article/' + article.id">
+                                {{ article.title }}
+                            </router-link>
+                        </a-menu-item>
+                    </a-sub-menu>
+                </a-menu>
+            </a-layout-header>
+            <a-layout-content><router-view /></a-layout-content>
+            <a-layout-footer :style="{ textAlign: 'center' }">
+                <p>
+                    <a
+                        href="http://www.beian.miit.gov.cn"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        鄂ICP备19010434号-1
+                    </a>
+                </p>
+
+                <p>
+                    <a
+                        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32011202000257"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        苏公网安备 32011202000257号
+                    </a>
+                </p>
+            </a-layout-footer>
+        </a-layout>
+
         <div style="padding: 3px" />
-        <router-view />
     </div>
 </template>
 <script>
 import Vue from "vue";
 import Component from "vue-class-component";
-import { Menu, Icon } from "ant-design-vue";
+import { Menu, Icon, Layout } from "ant-design-vue";
 import config from "@/config";
 import axios from "axios";
 export default
@@ -35,7 +63,12 @@ export default
         AMenu: Menu,
         AMenuItem: Menu.Item,
         ASubMenu: Menu.SubMenu,
-        AIcon: Icon
+        AIcon: Icon,
+        ALayout: Layout,
+        ALayoutHeader: Layout.Header,
+        ALayoutContent: Layout.Content,
+        ALayoutFooter: Layout.Footer,
+        ALayoutSider: Layout.Sider
     }
 })
 class App extends Vue {
@@ -56,20 +89,14 @@ class App extends Vue {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+    display: flex;
+    flex: 1 0 auto;
     text-align: center;
     color: #2c3e50;
 }
-
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #00000086;
-}
-
-#nav a.router-link-exact-active {
-    color: #4486e9;
+html,
+body {
+    height: 100%;
 }
 </style>
--- front-end/src/views/Article.vue
+++ front-end/src/views/Article.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="home">
-        <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-        <a-button type="primary">{{ JSON.stringify(article) }}</a-button>
+        <a-card v-bind:loading="loading" v-bind:title="title"
+            ><div v-html="compiledMarkdown"></div
+        ></a-card>
     </div>
 </template>
 
@@ -9,21 +10,43 @@
 // import { Prop } from "vue-property-decorator";
 import Vue from "vue";
 import Component from "vue-class-component";
-import { Button } from "ant-design-vue";
+import { Card } from "ant-design-vue";
 import config from "@/config";
 import axios from "axios";
+import marked from "marked";
 export default
-@Component({ components: { AButton: Button } })
+@Component({ components: { ACard: Card } })
 class Article extends Vue {
     // article = { id: this.$route.params.id };
     article = {};
+    loading = true;
     async created() {
-        console.log(config.host.backend);
-        this.article = (
-            await axios.get("/article/" + this.$route.params.id, {
-                baseURL: config.host.backend
-            })
-        ).data;
+        // console.log(config.host.backend);
+        try {
+            this.article = (
+                await axios.get("/article/" + this.$route.params.id, {
+                    baseURL: config.host.backend
+                })
+            ).data;
+        } catch (e) {
+            // console.log(JSON.stringify(e));
+            this.article = { title: "Error", content: "# 加载失败" };
+            // throw e;
+        }
+        this.afterLoad();
+    }
+    afterLoad() {
+        document.title = this.article.title;
+        this.loading = false;
+    }
+    get content() {
+        return this.article.content || "";
+    }
+    get title() {
+        return this.article.title || "";
+    }
+    get compiledMarkdown() {
+        return marked(this.content);
     }
 }
 </script>
