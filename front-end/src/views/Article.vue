2204a265926d8c53f9069eb1ac331caff1332892
HuGuang@NewDesktop <1395943920@qq.com>
2020-10-04 10:26:21 +0800

    Delete Some CRAP

--- front-end/src/views/Article.vue
+++ front-end/src/views/Article.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="home">
+        <div>{{ id }}</div>
+        <div>{{ this.$route.params.id }}</div>
         <markdown-render
             v-bind:loading="loading"
             v-bind:title="title"
             v-bind:content="content"
         />
+        <a-button-group block>
+            <a-button>Edit</a-button>
+            <a-button
+                type="danger"
+                v-if="!deleteConfirm"
+                v-on:click="deleteArticle"
+                >Delete</a-button
+            >
+            <a-button
+                type="danger"
+                v-if="deleteConfirm"
+                v-on:click="deleteArticle"
+                >Realy?</a-button
+            >
+        </a-button-group>
     </div>
 </template>
 
@@ -12,33 +29,66 @@
 // import { Prop } from "vue-property-decorator";
 import Vue from "vue";
 import Component from "vue-class-component";
+import { Button } from "ant-design-vue";
 import config from "@/config";
 import axios from "axios";
 import { MarkdownRender } from "@/components/MarkdownRender.vue";
 export default
-@Component({ components: { MarkdownRender } })
+@Component({
+    components: { MarkdownRender, AButton: Button, AButtonGroup: Button.Group }
+})
 class Article extends Vue {
     // article = { id: this.$route.params.id };
+    id = "";
     article = {};
     loading = true;
-    async created() {
-        // console.log(config.host.backend);
+    deleteConfirm = false;
+    created() {
+        this.fullLoad();
+    }
+    beforeUpdate() {
+        if (this.id !== this.$route.params.id) {
+            this.fullLoad();
+        }
+    }
+    async fullLoad() {
         try {
-            this.article = (
-                await axios.get("/article/" + this.$route.params.id, {
-                    baseURL: config.host.backend
-                })
-            ).data;
+            this.beforeLoad();
+            this.article = await this.loadArticle(this.id);
         } catch (e) {
-            // console.log(JSON.stringify(e));
+            console.log(e);
             this.article = { title: "Error", content: "# 加载失败" };
-            // throw e;
         }
         this.afterLoad();
+    }
+    beforeLoad() {
+        this.id = this.$route.params.id;
+        this.loading = true;
+    }
+    async loadArticle(id) {
+        this.loading = true;
+        let a = await axios.get("/article/" + id, {
+            baseURL: config.host.backend
+        });
+        console.log(a.data);
+        return a.data;
     }
     afterLoad() {
         document.title = this.article.title;
         this.loading = false;
+    }
+    async deleteArticle() {
+        if (!this.deleteConfirm) {
+            this.deleteConfirm = true;
+            setTimeout(() => (this.deleteConfirm = false), 1000);
+        } else {
+            let delRes = await axios.delete("/article/" + this.id, {
+                baseURL: config.host.backend
+            });
+            if (delRes.status == 200) {
+                this.$router.replace("/article");
+            }
+        }
     }
     get content() {
         return this.article.content || "";
